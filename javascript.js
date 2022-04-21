f5a474c1aa6d7dddefad30b983bfc8cc210c6401
artemis <arthur.toma@gmail.com>
2022-04-21 18:55:26 -0400

    Added winner declaration to game function

--- javascript.js
+++ javascript.js
@@ -39,28 +39,34 @@ function playRound(playerSelection, computerSelection) {
 }
 /*function to play 5 round game */
 function game(){
+    let playerScore = 0;
+    let computerScore = 0;
+
     for (let i = 0; i<5; i++){
         const playerSelection = changeToUpperCase(window.prompt('Rock, Paper, or Scissors?'));
         const computerSelection = computerPlay();
         console.log('You chose',playerSelection, 'and the computer chose',computerSelection);
         console.log(playRound(playerSelection, computerSelection));
+
         /**Point incrementation*/
         (declaration.startsWith('You win!'))? playerScore = playerScore +1 :
         (declaration.startsWith('You lose!'))? computerScore =  computerScore + 1 :
         'No point';
-        console.log( 'Player:', playerScore, 'Computer:', computerScore); 
-        
-    }
+        console.log( 'Player:', playerScore, 'Computer:', computerScore);  
+    };
+    /**Winner message */
+    let winnerMeassage = (playerScore > computerScore)?'The Player won!':
+    (playerScore < computerScore)?'The Computer won!': "It's a Draw..";
+
+    let finalScore = (`The final score is..\nPlayer: ${playerScore} Computer: ${computerScore}\n ${winnerMeassage}`);
+    return finalScore;
 }
-let playerScore = 0;
-let computerScore = 0;
+
 console.log(game());
 
-let finalScore = (`Player: ${playerScore} Computer: ${computerScore}`);
-let gameMessage = (playerScore > computerScore)?'The Player won!':
-(playerScore < computerScore)?'The Computer won!':
-"It's a Draw..";
-console.log(finalScore);
+
+
+
 
 
 

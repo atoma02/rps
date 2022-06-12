0961374170a069b04cce55737316ff9dd396916c
artemis <arthur.toma@gmail.com>
2022-06-12 14:28:43 -0400

    Added UI to rock, paper, scissors game

--- javascript.js
+++ javascript.js
@@ -1,68 +1,111 @@
 /*computer randomly chooses either rock, paper, or scissors*/
 function computerPlay(){
-const randomNumber = Math.floor(Math.random()*3)
-let computerChoice = (randomNumber === 0)? 'ROCK':
-    (randomNumber === 1)? 'PAPER':
-    (randomNumber === 2)? 'SCISSORS':
-    'Something is amiss';
-return computerChoice;
+    const randomNumber = Math.floor(Math.random()*3)
+    computerChoice = (randomNumber === 0)? 'ROCK':
+        (randomNumber === 1)? 'PAPER':
+        (randomNumber === 2)? 'SCISSORS':
+        'Something is amiss';
+    return computerChoice;
 }
 
 /*function to uppercase text */
-function changeToUpperCase(founder){
+/*function changeToUpperCase(founder){
     return founder.toUpperCase();
 }
+*/
+/*let playerPlay = (player)=> {
+    let playerSelection=player;   
+    const computerSelection = computerPlay();
+    console.log('You chose',playerSelection, 'and the computer chose',computerSelection);
+    const winner = (playRound(playerSelection, computerSelection));
+    return winner;
+}*/
 
 /*function to play one round of rock, paper, scissosrs*/
-function playRound(playerSelection, computerSelection) {
+let computerSelection;
+let declaration;
+function playRound(playerSelection) {
     /* playerSelection ROCK */
-    return declaration = (playerSelection == 'ROCK' && computerSelection =='PAPER')?
-        'You lose! Paper beats rock!':
+    computerSelection = computerPlay();
+    declaration = (playerSelection == 'ROCK' && computerSelection =='PAPER')?
+        'You lose! Paper beats Rock!':
         (playerSelection == 'ROCK' && computerSelection == 'SCISSORS')?
-        'You win! Rock beats scissors!':
+        'You win! Rock beats Scissors!':
         (playerSelection == 'ROCK' && computerSelection == 'ROCK')?
         'Draw!':
         /*playerSelection PAPER */
         (playerSelection == 'PAPER' && computerSelection == 'PAPER')?
         'Draw!':
         (playerSelection == 'PAPER' && computerSelection == 'ROCK')?
-        'You win! Paper beats rock!':
+        'You win! Paper beats Rock!':
         (playerSelection == 'PAPER' && computerSelection == 'SCISSORS')?
-        'You lose! Scissors beat paper!':
+        'You lose! Scissors beat Paper!':
         /*playerSelection SCISSORS*/
         (playerSelection == 'SCISSORS' && computerSelection == 'ROCK')?
-        'You lose! Rock beats scissors!':
+        'You lose! Rock beats Scissors!':
         (playerSelection == 'SCISSORS' && computerSelection == 'PAPER')?
-        'You win! Scissors beat paper!':
+        'You win! Scissors beat Paper!':
         (playerSelection == 'SCISSORS' && computerSelection == 'SCISSORS')?
         'Draw!':'Check your spelling..';
 }
-/*function to play 5 round game */
-function game(){
-    let playerScore = 0;
-    let computerScore = 0;
 
-    for (let i = 0; i<5; i++){
-        const playerSelection = changeToUpperCase(window.prompt('Rock, Paper, or Scissors?'));
-        const computerSelection = computerPlay();
-        console.log('You chose',playerSelection, 'and the computer chose',computerSelection);
-        console.log(playRound(playerSelection, computerSelection));
-
-        /**Point incrementation*/
-        (declaration.startsWith('You win!'))? playerScore = playerScore +1 :
-        (declaration.startsWith('You lose!'))? computerScore =  computerScore + 1 :
-        'No point';
-        console.log( 'Player:', playerScore, 'Computer:', computerScore);  
-    };
-    /**Winner message */
-    let winnerMeassage = (playerScore > computerScore)?'The Player won!':
-    (playerScore < computerScore)?'The Computer won!': "It's a Draw..";
-
-    let finalScore = (`The final score is..\nPlayer: ${playerScore} Computer: ${computerScore}\n ${winnerMeassage}`);
-    return finalScore;
+/**point incrementation*/
+let pointSystem = () =>{
+    (declaration.startsWith('You win!'))? playerScore = playerScore +1 :
+    (declaration.startsWith('You lose!'))? computerScore =  computerScore + 1 :
+    'No point';
 }
 
-console.log(game());
+/*disables button once score is reached*/
+let endGame = ()=> {
+    let buttons = document.querySelectorAll('button'); 
+    buttons.forEach((button) => {
+        button.disabled = true;
+    })
+    reset();
+}
+
+let reset = () => {
+    const resetBtn = document.createElement('button');
+    scoreBoard.appendChild(resetBtn);
+    resetBtn.textContent= 'Reset';
+    resetBtn.addEventListener('click', () => {
+        document.location.reload();
+    })
+}
+
+/*create rps buttons and function to play round */
+playerScore = 0;
+computerScore = 0;
+const container = document.querySelector('scoreBoard');
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        gameRound = playRound(button.id);
+
+        /*text to display each of the players choices*/
+        const gameText = `You chose ${button.id} and the Computer chose ${computerSelection}`
+        document.getElementById('scoreMessage').textContent = gameText;  
+
+        /*text to declare winner of the round*/ 
+        document.getElementById('roundText').textContent = declaration;
+        
+        /*point increment and text to update score*/
+        pointSystem();
+        document.getElementById('score').textContent = `Player: ${playerScore}
+                Computer: ${computerScore}`;
+                
+        /*Displays message declaring winner of first to 5*/
+        document.getElementById('winner').textContent= (playerScore == 5)? 'The Player has won!':
+        (computerScore == 5)? 'The Computer has won!':'';
+
+        if (playerScore == 5 || computerScore == 5) {
+            endGame();
+        }
+    });
+});
+
+
 
 
 
